85904dc1b17b2e318bd64089633157a7dda341d9
Puja Mukherjee <pujam836@gmail.com>
2023-02-07 21:36:45 -0800

    added php file for deployment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+// compile the template
+
 const fetchRecipes = () => {
-  fetch("http://localhost:8080/api/v1/recipe/DESSERT")
+  fetch("http://localhost:8080/api/v1/recipe/LUNCH")
     .then((response) =>
       response.json().then((data) => {
-        // console.log("hi");
+        console.log("hi");
         console.log(data);
-        console.log(data[0].name);
+        let recipeList = { recipes: data };
+        var source = document.getElementById("document-template").innerHTML;
+        var template = Handlebars.compile(source);
+        var html = template(data);
+        document.getElementById("recipeList").innerHTML = html;
+        //console.log(data[0].name);
       })
     )
     .catch((err) => console.log(err));
 };
 
+//document.addEventListener("click", fetchRecipes);
+
 document.addEventListener("DOMContentLoaded", fetchRecipes);
+
+// const registerHandlers = (event) => {
+//   const downArrow = document.querySelector('#down-arrow');
+//   downArrow.addEventListener('click', subtractDegree);
