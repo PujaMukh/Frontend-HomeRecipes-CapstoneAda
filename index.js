e2fc22932e1d77727ab79ae17f890ba327648abc
Puja Mukherjee <pujam836@gmail.com>
2023-02-09 14:11:40 -0800

    added some more css

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // compile the template
 
-const fetchRecipes = () => {
-  fetch("http://localhost:8080/api/v1/recipe/LUNCH")
+const fetchRecipesDessert = () => {
+  fetch("https://backend-recipe-capstone.herokuapp.com/api/v1/recipe/DESSERT")
     .then((response) =>
       response.json().then((data) => {
-        console.log("hi");
+        // console.log("hi");
         console.log(data);
         let recipeList = { recipes: data };
         var source = document.getElementById("document-template").innerHTML;
         var template = Handlebars.compile(source);
+        Handlebars.registerHelper("striped", function (index) {
+          return index % 2 === 0 ? "Green" : "Blue";
+        });
         var html = template(data);
         document.getElementById("recipeList").innerHTML = html;
         //console.log(data[0].name);
@@ -16,11 +19,59 @@ const fetchRecipes = () => {
     )
     .catch((err) => console.log(err));
 };
+document.addEventListener("DOMContentLoaded", fetchRecipesDessert);
 
-//document.addEventListener("click", fetchRecipes);
+// const fetchRecipesStarters = () => {
+//   fetch("https://backend-recipe-capstone.herokuapp.com/api/v1/recipe/SNACKS")
+//     .then((response) =>
+//       response.json().then((data) => {
+//         // console.log("hi");
+//         console.log(data);
+//         let recipeList = { recipes: data };
+//         var source = document.getElementById("document-template").innerHTML;
+//         var template = Handlebars.compile(source);
+//         Handlebars.registerHelper("striped", function (index) {
+//           return index % 2 === 0 ? "Green" : "Blue";
+//         });
+//         var html = template(data);
+//         document.getElementById("recipeList").innerHTML = html;
+//         //console.log(data[0].name);
+//       })
+//     )
+//     .catch((err) => console.log(err));
+// };
 
-document.addEventListener("DOMContentLoaded", fetchRecipes);
+// document.addEventListener("DOMContentLoaded", fetchRecipesStarters);
 
-// const registerHandlers = (event) => {
-//   const downArrow = document.querySelector('#down-arrow');
-//   downArrow.addEventListener('click', subtractDegree);
+// const fetchRecipesMeal = () => {
+//   fetch(
+//     "https://backend-recipe-capstone.herokuapp.com/api/v1/recipe/INDIAN_BENGALI"
+//   )
+//     .then((response) =>
+//       response.json().then((data) => {
+//         // console.log("hi");
+//         console.log(data);
+//         let recipeList = { recipes: data };
+//         var source = document.getElementById("document-template").innerHTML;
+//         var template = Handlebars.compile(source);
+//         Handlebars.registerHelper("striped", function (index) {
+//           return index % 2 === 0 ? "Green" : "Blue";
+//         });
+//         var html = template(data);
+//         document.getElementById("recipeList").innerHTML = html;
+//         //console.log(data[0].name);
+//       })
+//     )
+//     .catch((err) => console.log(err));
+// };
+
+// document.addEventListener("DOMContentLoaded", fetchRecipesMeal);
+
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
